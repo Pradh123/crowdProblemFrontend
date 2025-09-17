4422b98ef1b1ec081441a9fd49b15e02e3aabbce
Pradh123 <vermapradhumn3@gmail.com>
2025-09-17 21:27:00 +0530

    pradhumn vemra

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,15 @@
 import { defineConfig } from 'vite'
 import tailwindcss from '@tailwindcss/vite'
-
+import react from '@vitejs/plugin-react';
 export default defineConfig({
   plugins: [
     tailwindcss(),
+    react()
   ],
+  base: '/', // Adjust if deploying to a subdirectory, e.g., '/subpath/'
+  build: {
+    outDir: 'dist', // Output directory for build files
+    assetsDir: 'assets', // Directory for assets like images
+    sourcemap: false // Set to true for debugging, false for production
+  }
 })
