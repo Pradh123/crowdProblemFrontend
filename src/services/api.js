fe793b6f564934fb570219fc8bdad9906918a558
Pradh123 <vermapradhumn3@gmail.com>
2025-09-20 12:46:15 +0530

    pradhumn vemra

--- src/services/api.js
+++ src/services/api.js
@@ -1,8 +1,8 @@
 import axios from 'axios';
 
 const api = axios.create({
-  baseURL: 'http://localhost:5000/api',
-  // baseURL: 'https://crowdproblembackend-1.onrender.com/api',
+  // baseURL: 'http://localhost:5000/api',
+  baseURL: 'https://crowdproblembackend-1.onrender.com/api',
 });
 
 // Add a request interceptor to include the token from localStorage
